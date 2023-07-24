9371c1b2773deb2290ef7a373f58ce7f651b3bb3
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-07-24 11:07:06 -0300

    Comienzo de maquetacion de registro - componente - react

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -4,17 +4,22 @@ export default function Register() {
     // funciones - user form - destructuracion
     const {register, handleSubmit} = useForm()
     return(
-        <div>
-            <form  onSubmit={ handleSubmit(async (values)=> {
-                console.log(values);
-                const valueRegister = await registerRequest(values)
-                console.log(valueRegister);
-            })}>
-                <input type="text" name="username" {...register("username", {required:true})}/>
-                <input type="email" name="email" {...register("email", {required:true})}/>
-                <input type="password" name="password" {...register("password", {required:true})}/>
-                <button type="submit">Registar</button>
-            </form>
+        <div className="w-full flex justify-center">
+            <div className="w-[500px] bg-slate-400">
+            </div>
+            <div className="bg-slate-100 p-[10px]">
+                <h2 className="text-[35px] text-center text-orange-500 font-bold">Registro de cuenta</h2>
+                <form className="flex flex-col w-[500px]"  onSubmit={ handleSubmit(async (values)=> {
+                    console.log(values);
+                    const valueRegister = await registerRequest(values)
+                    console.log(valueRegister);
+                })}>
+                    <input className="bg-neutral-300 mt-[10px] pt-[10px] pb-[10px] px-[25px]" type="text" name="username" {...register("username", {required:true})}/>
+                    <input className="bg-neutral-300 mt-[10px] pt-[10px] pb-[10px] px-[25px]" type="email" name="email" {...register("email", {required:true})}/>
+                    <input className="bg-neutral-300 mt-[10px] pt-[10px] pb-[10px] px-[25px]"type="password" name="password" {...register("password", {required:true})}/>
+                    <button type="submit">Registar</button>
+                </form>
+            </div>
         </div>
     )
 }
